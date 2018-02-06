cf99b64fb79ad4235d1a7feebff26089aeecdf7f
David Weatherall <davidweatherall95@gmail.com>
2018-02-06 19:06:29 +0000

    updates

--- scripts/fbcalc.js
+++ scripts/fbcalc.js
@@ -1,12 +1,50 @@
+
+
 window.onload = function() {
 	createCalc();
 }
 
 function createCalc() {
 
+
+
 	$.get( "/data/firstBlood.json", function( data ) {
+		
 		calcHolder = $('.js-fbcalc');
-		calcHolder.html(data);
+
+		options = "<div class='each'><select class='js-select'>"
+
+		for (var i = 0; i < data.length; i++) {
+			option = "<option data-per=" + data[i]['fbPer'] + " data-sample=" + data[i]['sampleSize'] + ">" + data[i]['champName'] + "</option>"
+			options += option
+		}
+
+		options += "</select><div class='result'></div></div>";
+
+		i = 0;
+
+
+		structure = "<div class='half'>"
+
+		while(i < 5) {
+			structure += options
+			i++;
+		}
+
+		structure += "</div>"
+
+		structure += "<div class='half'>"
+
+		i = 0;
+
+		while(i < 5) {
+			structure += options
+			i++;
+		}
+
+		structure += "</div>"
+
+		calcHolder.html(structure);
 	});
 
 }
